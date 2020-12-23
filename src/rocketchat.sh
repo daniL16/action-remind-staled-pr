a97196acaa040a70f90749674edded0286c0f020
daniL16 <dani16lp@gmail.com>
2020-12-23 16:06:56 +0100

    test

--- src/rocketchat.sh
+++ src/rocketchat.sh
@@ -5,5 +5,6 @@ ROCKET_CHAT_URL="https://chat.bulevip.com"
 rocket::sendNotification(){
         local -r $message=$2
 	local -r prs=$(github::get_unactive_pr)
-	curl -H "Content-type:application/json" "{$ROCKET_CHAT_URl}hooks/{$ROCKET_CHAT_HOOK} -d '{"text": "$message", "username": "Github-Bot"}'
+	echo $prs
+	curl -H "Content-type:application/json" "{$ROCKET_CHAT_URl}/hooks/{$ROCKET_CHAT_HOOK}" -d '{"text": "$message", "username": "Github-Bot"}'
 }
