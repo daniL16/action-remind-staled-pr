ffdec8940512e80bddc684edc3004686d378b70c
daniL16 <dani16lp@gmail.com>
2020-12-23 16:26:35 +0100

    test

--- src/github.sh
+++ src/github.sh
@@ -7,7 +7,6 @@ github::get_unactive_pr(){
 	body=$(curl -sSL -H "$GITHUB_API_HEADER" -H "Authorization: token ${GITHUB_TOKEN}" "$GITHUB_API_URI/repos/$GITHUB_REPOSITORY/pulls")
 	pulls=$(echo "$body" | jq --raw-output '.[] | {date_upd: .updated_at, url: .html_url, draft: .draft, state: .state, title: .title } | @base64')
 	days=0
-	declare -a inactives
  	for p in $pulls; do
         	pull="$(echo "$p" | base64 -d)"
         	last_update=$(echo "$pull" | jq --raw-output '.date_upd')
@@ -15,9 +14,8 @@ github::get_unactive_pr(){
         	end_ts=$(date -d 'now' '+%s')
         	days_since_last_update=$(( ( end_ts - start_ts )/(60*60*24) ))
         	if [[ "$days_since_last_update" -ge "${INACTIVE_DAYS}" ]]; then
-           		$inactives+=$(echo "$pull" | jq --raw-output '(.title + "." + .url)')
-        	fi
+           		$message=$(echo "$pull" | jq --raw-output '(.title + "." + .url)')
+        		echo $message
+		fi
  	done
-
-	echo $inactives
 }
