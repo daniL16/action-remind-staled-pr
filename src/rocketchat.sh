9d2be905e801f341ad1b6c31faed42756ec86d4a
daniL16 <dani16lp@gmail.com>
2020-12-23 17:20:13 +0100

    test

--- src/rocketchat.sh
+++ src/rocketchat.sh
@@ -6,5 +6,5 @@ rocket::sendNotification(){
         local -r msg="$1"
 	data="{'text':'$msg','username': 'Github Bot'}"
 	echo $data
-	#curl -H "Content-type:application/json" "{$ROCKET_CHAT_URL}/hooks/{$ROCKET_CHAT_HOOK}" -d '{"text": "' + $msg + '", "username": "Github-Bot"}'
+	curl -H "Content-type:application/json" "{$ROCKET_CHAT_URL}/hooks/{$ROCKET_CHAT_HOOK}" -d $data
 }
