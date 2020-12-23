db658a9ff3c7714b25314ea8b6adb04bf320f7bc
daniL16 <dani16lp@gmail.com>
2020-12-23 16:13:22 +0100

    test

--- src/github.sh
+++ src/github.sh
@@ -11,12 +11,12 @@ github::get_unactive_pr(){
  	for p in $pulls; do
         	pull="$(echo "$p" | base64 -d)"
         	last_update=$(echo "$pull" | jq --raw-output '.date_upd')
-        	start_ts=$(date -d $last_update '+%s')
+        	start_ts=$(date -d '$last_update' '+%s')
         	end_ts=$(date -d 'now' '+%s')
         	days_since_last_update=$(( ( end_ts - start_ts )/(60*60*24) ))
         	if [[ "$days_since_last_update" -ge "$days" ]]; then
            		message=$(echo "$pull" | jq --raw-output '(.title + "." + .url)')
-           		inactive_pulls+=$message
+           		inactive_pulls+=$(echo "$message")
         	fi
  	done
 
